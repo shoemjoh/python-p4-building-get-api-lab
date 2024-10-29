7f9fa7167b817eed20fa68592a8e9273c038369c
John Shoemaker <shoemjoh@gmail.com>
2024-10-29 17:44:06 -0400

    working lab

--- server/app.py
+++ server/app.py
@@ -20,19 +20,48 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    print(bakeries)
+    bl = []
+    for bakery in bakeries:
+        bl.append(bakery.to_dict())
+    
+    return bl, 200
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+
+    if bakery:
+        body = bakery.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Bakery {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    print(baked_goods)
+
+    return [baked_good.to_dict() for baked_good in baked_goods], 200
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+
+
+    if baked_good:
+        body = baked_good.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Baked good not found.'}
+        status = 404
+
+    return make_response(body, status)
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
